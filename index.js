6348516e40a542392588c7b48f0c63a63f72763d
vais2004 <vkawale2004@gmail.com>
2025-12-23 00:20:51 +0530

    update: poslishing the code

--- index.js
+++ index.js
@@ -35,8 +35,9 @@ io.on("connection", (socket) => {
     const { sender, receiver, message } = data;
     const newMessage = new Messages({ sender, receiver, message });
     await newMessage.save();
+
+    socket.broadcast.emit("receive_message", data);
   });
-  socket.broadcast.emit("receive_message", data);
 
   socket.on("disconnect", () => {
     console.log("User disconnected", socket.id);
