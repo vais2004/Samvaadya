e3aee136be638d782c721ab16143d69fe58b99ea
vais2004 <vkawale2004@gmail.com>
2025-12-22 22:01:36 +0530

    update feat: wrote socket io logic, get route for messages and get route for users, schema for messages

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
+const http = require("http");
+const { Server } = require("socket.io");
+const Messages = require("./models/Messages");
+const { json } = require("stream/consumers");
+const User = require("./models/User");
+
 dotenv.config();
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5000",
+  },
+});
+
 app.use(cors());
 app.use(express.json());
 
@@ -14,6 +27,47 @@ mongoose
   .catch((error) => console.error(error));
 
 app.use("/auth", authRoutes);
+
+//socket io logic
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+
+  socket.on("send_message", async (data) => {
+    const { sender, receiver, message } = data;
+    const newMessage = new Messages({ sender, receiver, message });
+    await newMessage.save();
+  });
+  socket.broadcast.emit("receive_message", data);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
+application.get("/messages", async (req, res) => {
+  const { sender, receiver } = req.query;
+  try {
+    const messages = await Messages.find({
+      $or: [
+        { sender, receiver },
+        { sender: receiver, receiver: sender },
+      ],
+    }).sort({ createdAt: 1 });
+    res.json(messages);
+  } catch (error) {
+    res.status(500), json({ message: "Error while fetching messages." });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const { currentUser } = req.query;
+  try {
+    const users = await User.find({ username: { $ne: currentUser } });
+    res.json(users);
+  } catch (error) {
+    res.status(500), json({ message: "Error while fetching users" });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
