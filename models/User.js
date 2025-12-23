55f4cb2c6175dd67244a233c6f6ffb70460edfa9
vais2004 <vkawale2004@gmail.com>
2025-12-23 20:09:02 +0530

    update

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const UsersSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -17,9 +17,9 @@ const UsersSchema = new mongoose.Schema({
   },
 });
 
-UsersSchema.methods.comparePassword = async function (password) {
+UserSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const Users = mongoose.model("Users", UsersSchema);
-module.exports = Users;
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
